0f2228711fcb20b9e61ef33190e61a92c07f7e5f
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2016-02-15 19:09:58 +0530

    Added sign in with twitter functionality

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+//Replace XXX with your twitter application variables.
+define('CONSUMER_KEY', '--------------');
+define('CONSUMER_SECRET', '------------------------------');
+define('OAUTH_CALLBACK', 'http://site_url/twitter_login/new/process.php');
+?>
